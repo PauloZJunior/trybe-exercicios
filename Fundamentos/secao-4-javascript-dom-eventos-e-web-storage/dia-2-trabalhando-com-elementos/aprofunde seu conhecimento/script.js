d090a1d9d7ddd0e100cc0af51f885c907b590d7a
PauloZJunior <paulo_vga5@outlook.com>
2023-03-09 20:19:11 -0300

    exercicio 1 e 2 feitos

---
+++ Fundamentos/secao-4-javascript-dom-eventos-e-web-storage/dia-2-trabalhando-com-elementos/aprofunde seu conhecimento/script.js
@@ -0,0 +1,10 @@
+const mainTitle = document.createElement('h1');
+const mainContent = document.createElement('main');
+
+
+mainTitle.innerText = 'TrybeTrip - Agência de Viagens';
+document.body.appendChild(mainTitle);
+
+
+mainContent.className = 'main-content';
+document.body.appendChild(mainContent);
