bab19ba1f201cb1c20a80d4f8a53459ac9ef36f5
PauloZJunior <paulo_vga5@outlook.com>
2023-03-10 01:17:32 -0300

    exercicios do dia 2 da secao 4 feitos

--- Fundamentos/secao-4-javascript-dom-eventos-e-web-storage/dia-2-trabalhando-com-elementos/aprofunde seu conhecimento/script.js
+++ Fundamentos/secao-4-javascript-dom-eventos-e-web-storage/dia-2-trabalhando-com-elementos/aprofunde seu conhecimento/script.js
@@ -1,5 +1,11 @@
 const mainTitle = document.createElement('h1');
 const mainContent = document.createElement('main');
+const centerContent = document.createElement('section');
+const paragraph = document.createElement('p');
+const leftContent = document.createElement('section');
+const rightContent = document.createElement('section');
+const image = document.createElement('img');
+const uList = document.createElement('ul');
 
 
 mainTitle.innerText = 'TrybeTrip - Agência de Viagens';
@@ -8,3 +14,58 @@ document.body.appendChild(mainTitle);
 
 mainContent.className = 'main-content';
 document.body.appendChild(mainContent);
+
+
+centerContent.className = 'center-content';
+mainContent.appendChild(centerContent);
+
+
+paragraph.innerText = 'HELLO WORLD!'
+centerContent.appendChild(paragraph);
+
+
+leftContent.className = 'left-content';
+mainContent.appendChild(leftContent);
+
+
+rightContent.className = 'right-content';
+mainContent.appendChild(rightContent);
+
+
+image.src = 'https://picsum.photos/200';
+image.className = 'small-image';
+leftContent.appendChild(image);
+
+
+const numberNames = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+rightContent.appendChild(uList);
+for (let index = 0; index < numberNames.length; index += 1) {
+    const listItem = document.createElement('li');
+    listItem.innerText = numberNames[index];
+    uList.appendChild(listItem);
+}
+
+
+for (let index = 0; index < 3; index += 1) {
+    const titleH3 = document.createElement('h3');
+    titleH3.className = 'description';
+    titleH3.innerText = index + 1;
+    mainContent.appendChild(titleH3);
+}
+
+
+mainTitle.className = 'title';
+
+
+// const removeLeftContent = document.getElementsByClassName('left-content')[0];
+// mainContent.removeChild(removeLeftContent);
+
+
+rightContent.style.marginRight = 'auto';
+
+
+centerContent.parentNode.style.backgroundColor = 'green';
+
+
+uList.lastChild.remove();
+uList.lastChild.remove();
